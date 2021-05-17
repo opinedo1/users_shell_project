a236de5b95d721145a1fcb9488d1de32c35aadfd
Oscar Pinedo <opinedo19@gmail.com>
2021-05-17 02:02:23 -0700

    cleaned up spacing and added comments

--- users_shell/users_shell_app/views.py
+++ users_shell/users_shell_app/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import User
 
-# Create your views here.
+# Reads the data from our database and puts it in context
 def index(request):
     context = {
         'users': User.objects.all()
     }
     return render(request, 'index.html', context)
 
-def add_user(request):
-    print(request.POST)
 
+# Adds new users to our database that is received from our POST request
+def add_user(request):
     User.objects.create(
         first_name = request.POST['first_name'],
         last_name = request.POST['last_name'],
