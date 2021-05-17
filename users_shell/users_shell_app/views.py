8956dff6b7b85d459c7f18a2f5022b2c77662671
Oscar Pinedo <opinedo19@gmail.com>
2021-05-17 01:57:54 -0700

    add_user successfully adds new user data into our database

--- users_shell/users_shell_app/views.py
+++ users_shell/users_shell_app/views.py
@@ -10,4 +10,11 @@ def index(request):
 
 def add_user(request):
     print(request.POST)
+
+    User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email_address = request.POST['email_address'],
+        age = request.POST['age']
+    )
     return redirect('/')
