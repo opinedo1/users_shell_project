80ee0071497c4233d288eeeee0cfdf39f645630d
Oscar Pinedo <opinedo19@gmail.com>
2021-05-16 18:40:16 -0700

    added model for users

--- users_shell/users_shell_app/models.py
+++ users_shell/users_shell_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    frist_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
